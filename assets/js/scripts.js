1acac1146ce24114f4f3a6739949fb4d86dcac74
Raja Ziafat <rajaziafat3258@gmail.com>
2021-08-03 18:15:28 +0500

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
 	// Mobile Menu
 	$('.header_left a i').click(function(){
-		$('.footer_left_right').slideToggle();
+		$('.projects_footer').fadeIn(300);
+
+		return false
+	});
+	$('.projects_footer_respon_img2 img').click(function(){
+		$('.projects_footer').fadeOut(300);
 
 		return false
 	});
 
 	// Projects H1 Responsive
-	$('.project_h1_pos_rel,.project_h1_pos_rel2,.project_h1_pos_rel3').click(function(){
-		$('.projects_footer').show();
+	// $('.project_h1_pos_rel,.project_h1_pos_rel2,.project_h1_pos_rel3').click(function(){
+	// 	$('.projects_footer').show();
 
-		return false
-	});
-	$('.projects_footer_respon_img2 img').click(function(){
-		$('.projects_footer').hide();
+	// 	return false
+	// });
+	// $('.projects_footer_respon_img2 img').click(function(){
+	// 	$('.projects_footer').hide();
 
-		return false
-	});
+	// 	return false
+	// });
 
 	// Banner Carousel
 	// $('.banner_carousel').owlCarousel({
@@ -30,5 +35,12 @@ $(document).ready(function(){
 
 
 
-
+});
+window.addEventListener("load", async () => {
+  let video = document.querySelector("video[muted][autoplay]");
+  try {
+    await video.play();
+  } catch (err) {
+    video.controls = true;
+  }
 });
